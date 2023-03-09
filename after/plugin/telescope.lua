c12e48d81e9c005d466352d0dfee8b1c2c6e37e8
mshadow <medmystgon@gmail.com>
2023-03-09 18:12:35 +0100

    v1.2

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,6 +1,13 @@
+require('telescope').setup{
+  pickers = {
+    find_files = {
+      theme = "ivy",
+    }
+  },
+}
 local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
+vim.keymap.set('n', '<leader>f', builtin.find_files, {})
 vim.keymap.set('n', '<C-p>', builtin.git_files, {})
-vim.keymap.set('n', '<leader>ps',function() 
+vim.keymap.set('n', '<leader>ps',function()
 	builtin.grep_string({search = vim.fn.input("grep > ")});
 end)
