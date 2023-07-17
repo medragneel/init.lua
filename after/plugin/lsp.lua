20b03b6dea631134a82be0f3714e3455fa2e9afa
mshadow <medmystgon@gmail.com>
2023-07-17 11:15:16 +0100

    just added fugitive

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -51,7 +51,7 @@ lsp.on_attach(function(_, bufnr)
     local opts = { buffer = bufnr, remap = false }
 
 
-    vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+    vim.keymap.set("n", "dff", vim.lsp.buf.definition, opts)
     vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
     vim.keymap.set("n", "<leader>vws", vim.lsp.buf.workspace_symbol, opts)
     vim.keymap.set("n", "<leader>vd", vim.diagnostic.open_float, opts)
--- lua/medmh/packer.lua
+++ lua/medmh/packer.lua
@@ -4,7 +4,7 @@ return require('packer').startup(function(use)
     use {
         'nvim-telescope/telescope.nvim', tag = '0.1.0',
         -- or                            , branch = '0.1.x',
-        requires = { { 'nvim-lua/plenary.nvim' } , {'nvim-lua/popup.nvim'}}
+        requires = { { 'nvim-lua/plenary.nvim' }, { 'nvim-lua/popup.nvim' } }
     }
 
     -- use 'folke/tokyonight.nvim'
@@ -29,6 +29,9 @@ return require('packer').startup(function(use)
     }
     -- colorscheme
     use { "catppuccin/nvim", as = "catppuccin" }
+
+    -- git
+      use("tpope/vim-fugitive")
 
 
     --lspconfig
